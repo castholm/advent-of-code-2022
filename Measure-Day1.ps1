2c6cfddf07125b5c62c37ee923dd1fc234e795f0
Carl Åstholm <carl@astholm.se>
2022-12-01 10:49:24 +0100

    Day 1

--- Measure-Day1.ps1
+++ Measure-Day1.ps1
@@ -16,9 +16,18 @@ $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version 3.0
 
 function part1() {
+    $PuzzleInput -join ';' -split ';;'
+        | ForEach-Object { $_ -split ';' | Measure-Object -Sum }
+        | Measure-Object Sum -Maximum
+        | Select-Object -ExpandProperty Maximum
 }
 
 function part2() {
+    $PuzzleInput -join ';' -split ';;'
+        | ForEach-Object { $_ -split ';' | Measure-Object -Sum }
+        | Sort-Object Sum -Top 3 -Descending
+        | Measure-Object Sum -Sum
+        | Select-Object -ExpandProperty Sum
 }
 
 & "part$Part"
